684a03069cf399cba3b1b1a24e1f3094aa982f55
Little Mo <29153603+one-mo@users.noreply.github.com>
2019-04-06 18:49:45 +0800

    py3.6一键脚本

---
+++ python/CentOS_python3.6.sh
@@ -0,0 +1,10 @@
+#coding:utf-8
+export LANG=UTF-8
+export LANGUAGE=UTF-8
+yum groupinstall -y "Development tools"
+yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
+wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz && tar xvf Python-3.6.8.tar.xz && cd Python-3.6.8
+./configure
+make&& make install
+cd ..
+rm -rf Python-3.6.8 Python-3.6.8.tar.xz
