8128171ec18a15064e7ab7a1cabf6e52268adcf6
Little Mo <29153603+one-mo@users.noreply.github.com>
2019-04-06 19:19:41 +0800

    Create Jiacrontab.sh

---
+++ 某些项目部署/Jiacrontab.sh
@@ -0,0 +1,15 @@
+#coding:utf-8
+export LANG=UTF-8
+export LANGUAGE=UTF-8
+yum -y install unzip
+wget https://github.com/iwannay/jiacrontab/archive/v1.4.5.zip
+unzip jiacrontab-v1.4.5-linux-amd64.zip
+cd jiacrontab/server
+nohup ./jiaserver &> jiaserver.log &
+cd .. && cd client
+nohup ./jiaclient &> jiaclient.log &
+firewall-cmd --zone=public --add-port=20000-20003/tcp --permanent
+firewall-cmd --reload
+echo 安装完毕
+echo 访问：http://IP:20000
+echo 用户名为admin，密码为123456
