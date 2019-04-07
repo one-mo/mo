38befc84b261d6e4a0a4372deaae90395c2ab9ee
Little Mo <29153603+one-mo@users.noreply.github.com>
2019-04-07 08:17:22 +0800

    Update Jiacrontab.sh

--- 某些项目部署/Jiacrontab.sh
+++ 某些项目部署/Jiacrontab.sh
@@ -10,6 +10,8 @@ cd .. && cd client
 nohup ./jiaclient &> jiaclient.log &
 firewall-cmd --zone=public --add-port=20000-20003/tcp --permanent
 firewall-cmd --reload
+
+echo --------------------------
 echo 安装完毕
 echo 访问：http://IP:20000
-echo 用户名为admin，密码为123456
+echo 用户名：admin  密码：123456
